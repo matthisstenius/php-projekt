3d81bafb35cc3e64f9bb278c0c909a488d59f9ae
Matthis Stenius <matthis@me.com>
2013-10-21 13:46:15 +0200

    update to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
 		copy: {
 			build: {
 				cwd: 'src',
-				src: ['**'],
+				src: ['application/**', 'common/**', 'post/**', 'login/**', 'register/**'],
 				dest: 'build',
 				expand: true	
 			}
@@ -17,6 +17,25 @@ module.exports = function(grunt) {
 			}	
 		},
 
+		cssmin: {
+			minifyCSS: {
+				files: {
+					'build/public/css/main.css': 'src/public/css/*.css'
+				}
+			}
+		},
+
+		imagemin: {
+			minifyImg: {
+				files: [{
+					expand: true,
+					cwd: 'src',
+					src: ['public/img/*.{png,jpg,gif}'],
+					dest: 'build/public/img'
+				}]
+			}
+		},
+
 		watch: {
 			stylesheets: {
 				files: 'src/public/sass/main.scss',
@@ -28,8 +47,12 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-	grunt.registerTask('build', ['copy']);
+	grunt.registerTask('build', ['copy', 'minifyCSS', 'minifyImg']);
+	grunt.registerTask('minifyCSS', ['cssmin']);
+	grunt.registerTask('minifyImg', ['imagemin']);
 	grunt.registerTask('compileSass', ['sass']);
 	grunt.registerTask('default', 'Watches the project for changes', ['watch']);
 };
